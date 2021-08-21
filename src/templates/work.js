83c60b8d3417ea60405a02564c863f286b01590c
Radim Woznica <woznica@seznam.cz>
2021-08-21 15:26:20 +0200

    Update work.js

--- src/templates/work.js
+++ src/templates/work.js
@@ -11,6 +11,7 @@ export default ({ data }) => (
       <HelmetDatoCms seo={data.datoCmsWork.seoMetaTags} />
       <div className="sheet__inner">
         <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
+        <h1 className="sheet__title">{data.datoCmsWork.number}</h1>
         <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
         <div className="sheet__slider">
           <Slider infinite={true} slidesToShow={2} arrows>
